18c7f5fb98ac3deb57ecdcec2c9a1760bfb2acb1
Jason Butz <jbutz@users.noreply.github.com>
2015-12-07 08:59:44 -0500

    Add fallback when github doesn't have a name for the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
       user.provider = auth['provider']
       user.uid = auth['uid']
       if auth['info']
-         user.name = auth['info']['name'] || ""
+         user.name = auth['info']['name'] || auth['info']['nickname'] || ""
       end
     end
   end
